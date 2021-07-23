c269f8f7c3106db726250e430d9e88a6a6c00dca
Isreal6ix <isrealsix@gmail.com>
2021-07-24 00:20:15 +0700

    added function to detec number from speech, added event listeners on the replay button

--- JS_PROJ/Number Guessing | Seech recognition/index.js
+++ JS_PROJ/Number Guessing | Seech recognition/index.js
@@ -15,8 +15,47 @@ recognition.start();
 function onSpeak(ev) {
 	const msg = ev.results[0][0].transcript;
 
-	// writeMessage(msg);
-	// checkNumber(msg)
+	writeMessage(msg);
+	checkNumber(msg);
+}
+
+// Write what user speaks
+function writeMessage(msg) {
+	msgEl.innerHTML = `
+        <div>You said: </div>
+        <span class="box">${msg}</span>
+    `;
+}
+
+// Check msg aginst number
+function checkNumber(msg) {
+	const num = +msg;
+
+	//Check if valid number
+	if (Number.isNaN(num)) {
+		msgEl.innerHTML += '<div> That is not a valid number</div>';
+		return;
+	}
+
+	// Check in range
+
+	if (num > 100 || num < 1) {
+		msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+		return;
+	}
+
+	// Check number
+	if (num === randomNum) {
+		document.body.innerHTML = `
+        <h2>Congrats! You have guessed the number! <br /> <br />
+        It was ${num}</h2>
+        <button class="play-again" id="play-again">Play Again</button>
+        `;
+	} else if (num > randomNum) {
+		msgEl.innerHTML += '<div>GO LOWER</div>';
+	} else {
+		msgEl.innerHTML += '<div>GO HIGHER</div>';
+	}
 }
 
 // Generate random number
@@ -26,3 +65,12 @@ function getRandomNumber() {
 
 // Speak result
 recognition.addEventListener('result', onSpeak);
+
+// ENd SR service;
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', ev => {
+	if (ev.target.id == 'play-again') {
+		window.location.reload();
+	}
+});
